6c9e5102353f2f84423caa2b0349c87da3e4ee6e
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-10-05 15:20:16 -0700

    Still trying to find APIs

--- multi-api-react-app/src/Pages/Homepage.js
+++ multi-api-react-app/src/Pages/Homepage.js
@@ -1,60 +1,72 @@
 import React, { /* useState */ } from 'react'
 
-// I will have the plane API calculate how long a flight will be, then show movie(s) that are close to that length
+// Use Google Maps API to put in recipe ingredients and find stores nearby to buy them at
 function Homepage() {
-    // const [country, setCountry] = useState('US');
-    // const [currency, setCurrency] = useState('USD');
-    // const [language, setLanguage] = useState('en-US');
-    // const [startCity, setStartCity] = useState('New%20York');
-
-    // fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/${country.toUpperCase()}/${currency.toUpperCase()}/${language}/?query=${startCity}`, {
-    //     'method': 'GET',
-    //     'headers': {
-    //         'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-    //         'x-rapidapi-key': '39eb21bbd1msha190384ce6dd237p1d93c8jsn16d5c24c24db'
-    //     }
-    // })
+// Sets deck count
+    // fetch('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     // .then(response => {
-    //     console.log(response);
+    //     console.log('Deck of Cards');
     //     const name = response.json();
     //     console.log(name);
     // })
     // .catch(err => {
-    //     console.error(err);
-    // });
-
-    // fetch("http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/FR/eur/en-US/us/anywhere/anytime/anytime?apikey=prtl6749387986743898559646983194")
+    //     console.log(err);
+    // })
+// Draws cads
+    // fetch('http://deckofcardsapi.com/api/deck/new/draw/?count=2')
     // .then(response => {
-    //     console.log(response);
+    //     console.log('Drawn Cards');
+    //     const name = response.json();
+    //     console.log(name);
     // })
     // .catch(err => {
-    //     console.error(err);
-    // });
-
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO-sky/ORD-sky/2021-10-05?inboundpartialdate=2021-10-10", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "39eb21bbd1msha190384ce6dd237p1d93c8jsn16d5c24c24db"
-	}
-    })
+    //     console.log(err);
+    // })
+// Email Verification
+    // fetch('https://emailvalidation.abstractapi.com/v1/?api_key=2f48bbb070c647b891c70fb908139f55&email=xBarikadex@hotmail.com')
+    // .then(response => {
+    //         const name = response.json();
+    //         console.log(name);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // })
+// Gender Probability
+    // fetch('https://api.genderize.io?name=donald')
+    // .then(response => {
+    //         const name = response.json();
+    //         console.log(name);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // })
+// Recipes
+    fetch('https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=11ff8d9e&app_key=01250d39e0db773e22ad3860dcbfc9f9')
     .then(response => {
-        console.log(response);
-        const name = response.json();
-        console.log(name);
+            const name = response.json();
+            console.log(name);
     })
     .catch(err => {
-        console.error(err);
-    });
+        console.log(err);
+    })
 
     return (
         <div>
-            <p>Country (2 letter Max)</p>
-            <input type='text' minLength='2' maxLength='2' placeholder='US'/>
-            <p>Currency</p>
             
         </div>
     )
 }
 
 export default Homepage
+
+
+/* 
+    fetch('')
+    .then(response => {
+            const name = response.json();
+            console.log(name);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+*/
